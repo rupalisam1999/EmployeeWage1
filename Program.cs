183ed45d5ac8bbc66713edce32d905aeabb63ca6
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-09 20:51:51 +0530

    "ADDED: Calculate employee wage"

--- Program.cs
+++ Program.cs
@@ -8,15 +8,21 @@ namespace EmployeeWage1
         {
             int PRESENT = 0;
             int PARTIAL_PRESENT = 1;
+            int WAGE_PER_HOUR = 20;
+            int FULL_TIME_WORKING_HOUR = 8;
+            int wage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
             if (empCheck == 0)
             {
                 Console.WriteLine("Employee is present");
+                wage = FULL_TIME_WORKING_HOUR * WAGE_PER_HOUR;
+                Console.WriteLine($"WAGE:{wage}");
             }
             else if (empCheck == PARTIAL_PRESENT)
             {
                 Console.WriteLine("Employee is partial present");
+                Console.WriteLine($"WAGE:{wage}");
             }
             else
             {
