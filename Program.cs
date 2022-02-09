c215adcd10eb850877f30a85cced366f8582e361
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-09 21:06:21 +0530

    "ADDED: Part time employee and wage"

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace EmployeeWage1
             int PARTIAL_PRESENT = 1;
             int WAGE_PER_HOUR = 20;
             int FULL_TIME_WORKING_HOUR = 8;
+            int PART_TIME_WORKING_HOUR = 4;
             int wage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
@@ -22,6 +23,7 @@ namespace EmployeeWage1
             else if (empCheck == PARTIAL_PRESENT)
             {
                 Console.WriteLine("Employee is partial present");
+                wage = PART_TIME_WORKING_HOUR * WAGE_PER_HOUR;
                 Console.WriteLine($"WAGE:{wage}");
             }
             else
