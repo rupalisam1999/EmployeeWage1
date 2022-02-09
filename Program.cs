fb8e38f02ae18f1c538c0a6969db044cb2085d7a
rupalisam1999 <rupalisangale1999@gmail.com>
2022-02-09 16:41:58 +0530

    ADDED: Feature for employee check

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployeeWage1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 0)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+
+            }
+        }
+    }
+}
